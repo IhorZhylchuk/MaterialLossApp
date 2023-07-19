f93317d062e4a83454a4c5c69281af7eb58abc4f
IgorZhylchuk <ihor.zhylchuk@gmail.com>
2023-07-19 23:45:52 +0200

    Cteating project

---
+++ MaterialLossApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaterialLossApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
